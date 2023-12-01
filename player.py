fd9dab5723b464ea2104c6bbb105803e03fc354e
Benjamin Havens <benjamin.havens@advinow.com>
2023-11-30 19:47:03 -0600

    Ties and better UI in player.

--- player.py
+++ player.py
@@ -30,7 +30,8 @@ class TicTacToeGamePlayer:
     Plays TicTacToe between two opponents. The strategy can be different between X and O.
     """
 
-    def __init__(self, player_X=get_human_tictactoe_move, player_O=get_human_tictactoe_move, silent=False):
+    def __init__(self, player_X=get_human_tictactoe_move, player_O=get_human_tictactoe_move, player_X_name=None,
+                 player_O_name=None, silent=False):
         """
         Initialize the player with two players.
         :param player_X: Callable which will receive a TicTacToeBoard as input and should return a tuple
@@ -42,25 +43,45 @@ class TicTacToeGamePlayer:
         """
         self.player_X = player_X
         self.player_O = player_O
+        self.player_X_name = player_X_name or str(player_X)
+        self.player_O_name = player_O_name or str(player_O)
         self.silent = silent
         self.board = TicTacToeBoard()
 
     def play(self):
+        # Make rules clear.
+        self._maybe_print("Press enter at each stage to move on.\n"
+                          f"Beginning a game between {self.player_X_name} as X and {self.player_O_name} as O!",
+                          clear=False)
+
         player, next_player = self.player_X, self.player_O
+        player_name, next_player_name = self.player_X_name, self.player_O_name
         while not self.board.is_game_over:
+            # Get and announce move
             self._maybe_print(self.board)
             move = player(self.board)
-            self._maybe_print(f"Player {self.board.next_player.name} chose to play at {move[0] + 1, move[1] + 1}.")
+            self._maybe_print(f"Player {self.board.next_player.name}, using strategy {player_name}, "
+                              f"chose to play at {move[0] + 1, move[1] + 1}.")
             self.board.play_move(*move)
+
+            # Swap roles
             player, next_player = next_player, player
-        self._maybe_print(f"Player {self.board.winning_player.name} wins!")
+            player_name, next_player_name = next_player_name, player_name
+        self._maybe_print(self.board, clear=False)
+        if self.board.winning_player is not None:
+            self._maybe_print(f"Player {self.board.winning_player.name} wins!", clear=False)
+        else:
+            self._maybe_print("It's a tie!", clear=False)
         self.board.reset()
 
-    def _maybe_print(self, s):
+    def _maybe_print(self, s, clear=True):
         if not self.silent:
-            require_input_and_clear(s)
+            if clear:
+                require_input_and_clear(s)
+            else:
+                print(s)
 
 
 if __name__ == "__main__":
-    player = TicTacToeGamePlayer(player_O=get_best_tictactoe_move)
+    player = TicTacToeGamePlayer(player_O=get_best_tictactoe_move, player_X_name="Human", player_O_name="MiniMax")
     player.play()
