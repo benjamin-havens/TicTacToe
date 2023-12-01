ecbe123f65e7c935807c70ce273d620d6afd4226
Benjamin Havens <benjamin.havens@advinow.com>
2023-11-30 20:38:42 -0600

    Refactored for better code organization

--- player.py
+++ player.py
@@ -1,28 +1,7 @@
 from board import TicTacToeBoard
-from minimax import get_best_tictactoe_move
 from printing_utils import require_input_and_clear
-
-
-def get_human_tictactoe_move(board: TicTacToeBoard):
-    valid_moves = board.get_possible_moves()
-
-    while True:
-        user_row = require_input_and_clear("Enter row to play on: ")
-        user_column = require_input_and_clear("Enter column to play on: ")
-
-        # Check integers
-        try:
-            user_row, user_column = int(user_row), int(user_column)
-        except ValueError:
-            require_input_and_clear("Invalid entry: enter integers between 1 and 3. Press enter to continue.)")
-            continue
-
-        # Check valid moves
-        if (user_row - 1, user_column - 1) not in valid_moves:
-            require_input_and_clear("Invalid move: ensure the chosen square is empty. Press enter to continue.")
-            continue
-
-        return user_row - 1, user_column - 1
+from strategies.human import get_human_tictactoe_move
+from strategies.minimax import get_best_tictactoe_move
 
 
 class TicTacToeGamePlayer:
---
+++ strategies/human.py
@@ -0,0 +1,20 @@
+def get_human_tictactoe_move(board: TicTacToeBoard):
+    valid_moves = board.get_possible_moves()
+
+    while True:
+        user_row = require_input_and_clear("Enter row to play on: ")
+        user_column = require_input_and_clear("Enter column to play on: ")
+
+        # Check integers
+        try:
+            user_row, user_column = int(user_row), int(user_column)
+        except ValueError:
+            require_input_and_clear("Invalid entry: enter integers between 1 and 3. Press enter to continue.)")
+            continue
+
+        # Check valid moves
+        if (user_row - 1, user_column - 1) not in valid_moves:
+            require_input_and_clear("Invalid move: ensure the chosen square is empty. Press enter to continue.")
+            continue
+
+        return user_row - 1, user_column - 1
