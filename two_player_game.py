d08049708e2b0e47248274458e5ec63e83405989
Benjamin Havens <benjamin.havens@advinow.com>
2023-12-04 14:07:57 -0600

    Move silent param to terminal fn in game player

--- two_player_game.py
+++ two_player_game.py
@@ -135,8 +135,7 @@ class TwoPlayerGame:
     def __init__(self,
                  board: TwoPlayerGameBoard,
                  player_1_agent: TwoPlayerGameAgent,
-                 player_2_agent: TwoPlayerGameAgent,
-                 silent=False):
+                 player_2_agent: TwoPlayerGameAgent):
         """
         Initialize the game with two players.
         :param board: TwoPlayerGameBoard of the game to be played.
@@ -149,34 +148,34 @@ class TwoPlayerGame:
         self.board = board
         self.player_1 = player_1_agent
         self.player_2 = player_2_agent
-        self.silent = silent
 
     def swap_player_1_and_player_2(self):
         self.player_1, self.player_2 = self.player_2, self.player_1
 
-    def play_in_terminal(self):
+    def play_in_terminal(self, silent=False):
         self.board.reset()
         self._maybe_print("Press enter at each stage to move on.\n"
                           f"Beginning a game of {self.board.name} between {self.player_1.name} and {self.player_2.name}!\n",
+                          silent=silent,
                           clear=False)
 
         # Main game loop
         player, next_player = self.player_1, self.player_2
         while not self.board.is_game_over:
             # Get and announce move
-            self._maybe_print(self.board)
+            self._maybe_print(self.board, silent=silent)
             move = player.get_move(self.board)
-            self._maybe_print(f"Player {player.name} played {move}.")
+            self._maybe_print(f"Player {player.name} played {move}.", silent=silent)
             self.board.play_move(move)
 
             # Swap roles
             player, next_player = next_player, player
 
-        self._maybe_print(self.board, clear=False)
+        self._maybe_print(self.board, silent=silent, clear=False)
         if self.board.winning_player != "":
-            self._maybe_print(f"Player {self.board.winning_player} wins!\n", clear=False)
+            self._maybe_print(f"Player {self.board.winning_player} wins!\n", silent=silent, clear=False)
         else:
-            self._maybe_print("It's a tie!\n", clear=False)
+            self._maybe_print("It's a tie!\n", silent=silent, clear=False)
 
     def play_in_GUI(self, gui: TwoPlayerGameGUI):
         self.board.reset()
@@ -201,8 +200,9 @@ class TwoPlayerGame:
         gui.display_winner(self.board.winning_player)
         gui.await_exit()
 
-    def _maybe_print(self, s, clear=True):
-        if not self.silent:
+    @staticmethod
+    def _maybe_print(s, silent=False, clear=True):
+        if not silent:
             if clear:
                 require_input_and_clear(s)
             else:
